886531b42e721c78865587f72af704e1b53e704a
Nhat Nguyen <77106876+trollsad125-dev@users.noreply.github.com>
2026-04-26 21:08:51 +0700

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i =0;i<s.length();i++){
+           boolean b = true;
+            char c = s.charAt(i);
+            switch (c){
+                case '{':
+                case '(':
+                case '[':
+                    stack.push(c);
+                    break;
+                case '}':
+                    if(stack.isEmpty()) return false;
+                    b =  stack.pop()  == '{';
+                    if(!b) return false;
+                    break;
+                case ')':
+                    if(stack.isEmpty()) return false;
+                    b = stack.pop()  == '(';
+                    if(!b) return false;
+                    break;
+                case ']':
+                    if(stack.isEmpty()) return false;
+                    b = stack.pop()  == '[';
+                    if(!b) return false;
+                    break;     
+            }
+        }
+        return stack.isEmpty();
+    }
+}

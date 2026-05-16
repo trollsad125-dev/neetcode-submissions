cf4a1643e0b6c5a73d01aaf7894b1f6b118dea66
Nhat Nguyen <77106876+trollsad125-dev@users.noreply.github.com>
2026-05-16 18:03:21 +0700

    Add: minimum-size-subarray-sum - submission-15

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-15.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int l = 0, total = 0, r = 0;
+        Integer minLength = null;
+        while (r < nums.length) {
+            if (total >= target) {
+                int length = r - l;
+                minLength = (minLength == null) ? length : Math.min(minLength, length);
+                total -= nums[l];
+                l++;
+            } else {
+                total += nums[r];
+                r++;
+            }
+        }
+        while (total >= target) {
+            total -= nums[l];
+            minLength = minLength == null ? r-l : Math.min(minLength, r - l);
+            if (total >= target) {
+                l++;
+            }
+        }
+        return minLength == null ? 0: minLength;
+    }
+}
